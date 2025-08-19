816e00966e0a9b342ba63c5e283a9d9416cf9c14
mahakPandeyStartappss <mahak0312pandey@gmail.com>
2025-08-19 18:32:49 +0530

    Day1 Updates

--- Day1/TwoSum.js
+++ Day1/TwoSum.js
@@ -58,7 +58,45 @@ console.log(twoSumByMap(nums, target));
 
 /*
 
-EXPLANATON:
+EXPLAINATION:
+
+Step by Step Explanation for Optimized:
+
+1. Map banate hain → const map = new Map();
+
+       - Ye ek storage hai jisme hum number → index rakhte hain.
+
+       - Matlab agar humne pehle koi number dekha hai, to uska index map me save kar denge.
+
+2. Loop start karte hain (for (let i = 0; i < nums.length; i++))
+
+       -   Har element check karenge ek ek karke.
+
+3. Complement nikalte hain → let complement = target - nums[i];
+
+         -   Matlab, agar mujhe target banana hai, aur mere paas nums[i] hai,
+            to mujhe aur kaunsa number chahiye? → complement.
+
+         -   Example:
+
+         -   Agar target = 9 aur nums[i] = 2 hai → complement = 9 - 2 = 7.
+
+         -   Matlab mujhe 7 chahiye to pair banega.
+
+4. Check karte hain map me → if (map.has(complement))
+
+          -  Agar ye complement pehle array me aa chuka hai (aur map me store hai),
+            to iska matlab pair mil gaya.
+
+5. Return karo → [map.get(complement), i]
+
+           - map.get(complement) = uska purana index
+
+           -    i = current index
+
+6. Nahi mila to store kar do → map.set(nums[i], i);
+
+`           -    Agar abhi tak pair nahi mila, to current number ko map me daal do future ke liye.
 
 
 
